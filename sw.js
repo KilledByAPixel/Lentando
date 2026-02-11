bf2b251046cb4dd591db1fb2a210c903d9dc46ae
Frank Force <frankforce@gmail.com>
2026-02-11 15:00:19 -0600

    lentando-v134

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Lentando PWA
 const SW_DEBUG = false; // Set to true to enable console logging
-const CACHE_NAME = 'lentando-v133';
+const CACHE_NAME = 'lentando-v134';
 const urlsToCache = [
   './index.html',
   './code.js',
