b38eadef7ba372aae051d09df659fc82411087a9
Frank Force <frankforce@gmail.com>
2026-02-12 17:16:20 -0600

    fix: use currentDate() for default date to respect debug time offset

--- code.js
+++ code.js
@@ -2412,8 +2412,8 @@ function openCreateEventModal() {
 
   const fieldsHTML = fields.map(modalFieldWrap).join('');
 
-  // Default to now
-  const nowDate = new Date();
+  // Default to now (use currentDate() so debug time offset is respected)
+  const nowDate = currentDate();
   const dateValue = `${nowDate.getFullYear()}-${String(nowDate.getMonth() + 1).padStart(2, '0')}-${String(nowDate.getDate()).padStart(2, '0')}`;
   const timeValue = `${String(nowDate.getHours()).padStart(2, '0')}:${String(nowDate.getMinutes()).padStart(2, '0')}`;
 
